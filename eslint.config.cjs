796f0ed34212fb1202446a91774b9eab1379fd75
Xtracejaz16 <sebastian.estradab@comunidad.iush.edu.co>
2026-05-21 12:40:05 -0500

    fix(eslint): use parser object in eslint.config.cjs for flat config

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -2,7 +2,7 @@ module.exports = [
   {
     files: ['**/*.{js,cjs,mjs,ts,tsx}'],
     languageOptions: {
-      parser: require.resolve('@typescript-eslint/parser'),
+      parser: require('@typescript-eslint/parser'),
       parserOptions: { project: ['./tsconfig.json'] }
     },
     plugins: { '@typescript-eslint': require('@typescript-eslint/eslint-plugin') },
